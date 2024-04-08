9fdaf28b1b813f7e1c2617ddc6cfef5b4322e51f
unknown <crystalbritish1120@gmail.com>
2024-04-08 16:59:54 +0900

    0408

--- src/views/Room.vue
+++ src/views/Room.vue
@@ -13,6 +13,10 @@ import { ref, onMounted } from "vue";
 
 const audios = ref([]);
 const videos = ref([]);
+
+const selectedAudio = ref();
+const selectedVideo = ref();
+const vidoeStream = ref();
 onMounted(async () => {
   const deviceInfos = await navigator.mediaDevices.enumerateDevices();
   //   Get Audio Device Information
@@ -35,45 +39,25 @@ onMounted(async () => {
     );
   console.log("audio Info", audios.value);
   console.log("camera Info", videos.value);
+  try {
+    const video = document.getElementById("#video");
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true,
+    });
+    video.srcObject = vidoeStream;
+    console.log("detect the camera");
+  } catch (e) {
+    console.log("not found");
+  }
 });
 </script>
 
 <template>
   <div>
     <h1 class="text-red-500 font-bold text-lg">Room</h1>
-    <!-- <div class="room-content">
-      <video id="their-video" width="200" autoplay playsinline></video>
-      <video id="my-video" muted="true" width="500" autoplay playsinline></video>
-      <p>
-        Your Peer ID: <span id="my-id">{{ peerId }}</span>
-      </p>
-      <input v-model="calltoid" placeholder="call id" />
-      <button @click="makeCall" class="button--green">Call</button>
-      <br />
-
-      マイク:
-      <select v-model="selectedAudio" @change="onChange">
-        <option disabled value="">Please select one</option>
-        <option
-          v-for="(audio, key, index) in audios"
-          v-bind:key="index"
-          :value="audio.value"
-        >
-          {{ audio.text }}
-        </option>
-      </select>
-
-      カメラ:
-      <select v-model="selectedVideo" @change="onChange">
-        <option disabled value="">Please select one</option>
-        <option
-          v-for="(video, key, index) in videos"
-          v-bind:key="index"
-          :value="video.value"
-        >
-          {{ video.text }}
-        </option>
-      </select>
-    </div> -->
+    <div class="w-96 h-77">
+      <video id="video"></video>
+    </div>
   </div>
 </template>
